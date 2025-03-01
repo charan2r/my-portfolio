8275653363fc881bc3021b42076b2170e4367865
Charan Romi <charanromi325@gmail.com>
2025-03-01 21:57:25 +0530

    new updates

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,20 +1,19 @@
 import ProjectInfo from './ProjectInfo';
 import Healthcare from '../assets/healthcare.jpg';
 import Todo from '../assets/todo.png';
-import Calculator from '../assets/calculator.png';
 import ImageToText from '../assets/converter.png';
 import Ecommerce from '../assets/ecommerce.png';
 import Logbook from '../assets/logbook.png';
 import Forms from '../assets/forms.png';
-import User from '../assets/user.png';
+import Apppointment from '../assets/appointment.png';
 
 const Projects = () => {
     const projectList = [
         {
             title: "E-commerce Site",
             description:
-                "A full-stack healthcare management system designed to manage appointment bookings and improve disease awareness among people by giving information about diseases. Built using HTML, CSS, JavaScript, PHP, and MySQL, this project offers a user-friendly interface for both patients and healthcare providers. ",
-            github: "https://github.com/charan2r/e-commerce-site.git",
+                "Developed an e-commerce website using MERN Stack to provide a seamless shopping experience for customers. The site features a user-friendly interface, product categorization, search functionality, and a secure payment gateway. ",
+            github: "https://github.com/charan2r/aura-store.git",
             demo: "",
             photo: Ecommerce,
         },
@@ -37,7 +36,7 @@ const Projects = () => {
         {
             title: "Healthcare Management System",
             description:
-                "A full-stack healthcare management system designed to manage appointment bookings and improve disease awareness among people by giving information about diseases. Built using HTML, CSS, JavaScript, PHP, and MySQL, this project offers a user-friendly interface for both patients and healthcare providers. ",
+                "A healthcare management system designed to manage appointment bookings and improve disease awareness among people by giving information about diseases. Built using HTML, CSS, JavaScript, PHP, and MySQL, this project offers a user-friendly interface for both patients and healthcare providers. ",
             github: "https://github.com/charan2r/Healthcare-Management.git",
             demo: "",
             photo: Healthcare,
@@ -51,27 +50,19 @@ const Projects = () => {
             photo: Todo,
         },
         {
-            title: "User Authentication System",
-            description:
-                "A simple user authentication system that allows users to register, log in, and log out. After login, user is directed to a simple landing page. The system is built using MERN stack. It includes features like password hashing, JWT-based authentication, and user session management. ",
-            github: "https://github.com/charan2r/authentication.git",
-            photo: User,
-        },
-        {
-            title: "Calculator App",
-            description:
-                "A React-based application that combines a simple calculator and an age calculator. The app is designed to provide accurate calculations with an intuitive interface.",
-            github: "https://github.com/charan2r/calculator-app.git",
-            demo: "https://calculator-app-chi-ruby.vercel.app/",
-            photo: Calculator,
-        },
-        {
             title: "Image to Text Converter",
             description:
                 "A React application that utilizes Tesseract.js, an OCR (Optical Character Recognition) library, to extract text from images. ",
             github: "https://github.com/charan2r/converter.git",
             demo: "https://image-to-text-converter-liart.vercel.app/",
             photo: ImageToText,
+        },
+        {
+            title: "Appointment Booking System",
+            description: "Developed a full-stack appointment booking application using PERN Stack that allows users to view available time slots, book appointments, and manage their bookings. ",
+            github: "https://github.com/charan2r/appointment-booking-system.git",
+            demo: "",
+            photo: Apppointment,
         },
     ];
     
