3e7664c4773846c86bd8b91af99d64bf7c679019
Charan Romi <charanromi325@gmail.com>
2025-03-18 20:08:26 +0530

    new changes

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,16 +1,13 @@
 import ProjectInfo from './ProjectInfo';
 import Healthcare from '../assets/healthcare.jpg';
-import Todo from '../assets/todo.png';
-import ImageToText from '../assets/converter.png';
 import Ecommerce from '../assets/ecommerce.png';
 import Logbook from '../assets/logbook.png';
 import Forms from '../assets/forms.png';
-import Apppointment from '../assets/appointment.png';
 
 const Projects = () => {
     const projectList = [
         {
-            title: "E-commerce Site",
+            title: "Aura Store",
             description:
                 "Developed an e-commerce website using MERN Stack to provide a seamless shopping experience for customers. The site features a user-friendly interface, product categorization, search functionality, and a secure payment gateway. ",
             github: "https://github.com/charan2r/aura-store.git",
@@ -20,7 +17,7 @@ const Projects = () => {
         {
             title: "Digital Logbook",
             description:
-                "Developed a system as a group project to track, review, and grade daily activities for students and mentors. My contributions included implementing a robust testing framework to enhance system reliability, creating a feedback component to improve mentor-student interactions, and building backend functionality for bulk report exports to streamline academic evaluations. ",
+                "Developed a logbook system as a group project to track, review, and grade daily activities for students and mentors. My contributions included implementing a robust testing framework to enhance system reliability, creating a feedback component to improve mentor-student interactions, and building backend functionality for bulk report exports to streamline academic evaluations. ",
             github: "",
             demo: "",
             photo: Logbook,
@@ -41,29 +38,7 @@ const Projects = () => {
             demo: "",
             photo: Healthcare,
         },
-        {
-            title: "To-Do App",
-            description:
-                "A full-stack to-do list application that allows users to manage their tasks efficiently. Developed using HTML5, Tailwind CSS, JavaScript, Node.js, Express, and SQLite, this app integrates essential features like user authentication, CRUD operations, task expiration notifications, and a search function for locating tasks. ",
-            github: "https://github.com/charan2r/todo-app.git",
-            demo: "",
-            photo: Todo,
-        },
-        {
-            title: "Image to Text Converter",
-            description:
-                "A React application that utilizes Tesseract.js, an OCR (Optical Character Recognition) library, to extract text from images. ",
-            github: "https://github.com/charan2r/converter.git",
-            demo: "https://image-to-text-converter-liart.vercel.app/",
-            photo: ImageToText,
-        },
-        {
-            title: "Appointment Booking System",
-            description: "Developed a full-stack appointment booking application using PERN Stack that allows users to view available time slots, book appointments, and manage their bookings. ",
-            github: "https://github.com/charan2r/appointment-booking-system.git",
-            demo: "",
-            photo: Apppointment,
-        },
+        
     ];
     
 
@@ -77,6 +52,11 @@ const Projects = () => {
                 {projectList.map((project, index) => (
                     <ProjectInfo key={index} project={project} />
                 ))}
+            </div>
+            <div className="projectButton">
+                <a href="https://github.com/charan2r" target="_blank" rel="noreferrer">
+                    See More...
+                </a>
             </div>
         </div>
     );
